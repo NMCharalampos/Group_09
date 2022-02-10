662732be5e6989059697030ae9ba46d6ae4cbfdf
NMCharalampos <nicolas.moissiadis@gmail.com>
2022-02-10 15:02:50 +0000

    Add Downloads Function NicolasM

--- DataHandler.py
+++ DataHandler.py
@@ -1,8 +1,17 @@
+import requests
+import pandas as pd
+import os
 
 
-
-
-class DataHandler: 
+class DataHandler:
 
     def __init__(self):
-        
+        pass
+
+    def download(self, x):
+        r = requests.get(x)
+        file_content = r.text
+        directory = os.path.join('downloads', 'Consumption.csv')
+        os.makedirs(os.path.dirname(directory), exist_ok=True)
+        with open(directory, "w") as f:
+            f.write(file_content)
