ce94fa086c485cddceca42778dfe316e43116cef
strassl-gif <51350@novasbe.pt>
2022-02-18 16:55:20 +0000

    *inserted method 4+5; included documentation draft for each*

--- DataHandler.py
+++ DataHandler.py
@@ -1,12 +1,28 @@
 import os
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 
 DATA_URL = "https://raw.githubusercontent.com/owid/energy-data/master/owid-energy-data.csv"
 DIRECTORY = os.path.join('downloads', 'Consumption.csv')
 
 class DataHandler:
+
+    """
+    ...TBD...
+    
+    Methods
+    --------
+    plot_consumption2()
+        Plots the total sum of each energy consumption column in dataframe 'df' for
+        countries selected in '*args' as a bar chart.
+
+    gdp()
+        Plots the GDP column of dataframe 'df' over the years for
+        countries selected in '*args' as a line chart.
+
+    """
 
     data = pd.DataFrame
 
@@ -33,7 +49,7 @@ class DataHandler:
     def list_countries(self):
         return [country for country in self.data.country.unique()]
 
-    def plot_consumption(self, country, normalize=False):
+    def plot_consumption1(self, country, normalize=False):
         if not self.is_country(country):
             return ValueError("This country does not exist.")
 
@@ -48,9 +64,65 @@ class DataHandler:
     def is_country(self, country):
         return country in self.list_countries()
 
-# hallo Test
+    def plot_consumption2(df,*arg): # To-Do: Name of "dfNew" (Nico's df2017)
+        """
+
+        Plots the total sum of each energy consumption column in dataframe 'df' for
+        countries selected in '*args' as a bar chart.
+
+        Parameters
+        ---------------
+        df: pd.DataFrame()
+            The dataframe containing the energy consumption columns per country
+        *arg: string
+            Countries that shall be plotted
+
+        Returns
+        ---------------
+        Nothing. Plots sum of different energy consumption per country in a bar chart.
+
+        """
+        consumption = pd.DataFrame()
+        countries_list = []
+        for x in arg:
+            countries_list.append(x)
+            dfc = df.loc[df["country"] == x].filter(regex='consumption').sum()
+            consumption = consumption.append(dfc, ignore_index = True)
+            consumption.index = countries_list
+        ax2 = consumption.plot.bar(rot=0)
+        #ax2.yaxis.set_major_formatter(mtick.PercentFormatter()) --> why %?
+        ax2.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+        #print(consumption)
+
+    def gdp(df, *arg):
+        """
+
+        Plots the GDP column of dataframe 'df' over the years for
+        countries selected in '*args' as a line chart.
+
+        Parameters
+        ---------------
+        df: pd.DataFrame()
+            The dataframe containing the GDP per country over the years
+        *arg: string
+            Countries that shall be plotted
+
+        Returns
+        ---------------
+        Nothing. Plots GDP over the years per country in a line chart.
+
+        """
+        df['year'] = df.index
+        for x in arg:
+            df_gdp = df.loc[df["country"] == x][['country','gdp','year']]
+            plt.plot(df_gdp['year'],df_gdp['gdp'], label = x)
+        plt.title('GDP Development')
+        plt.xlabel('Year')
+        plt.ylabel('GDP (in billion USD)')
+        plt.legend()
+        plt.show()
 
 dataHandler = DataHandler()
 dataHandler.load_data()
 
-dataHandler.plot_consumption('Kosovos')
+dataHandler.plot_consumption1('Kosovos')
